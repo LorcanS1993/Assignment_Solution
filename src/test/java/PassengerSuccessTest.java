084cdf0c15e315b5e9a0d9676d320453d7b3447f
Lorcan1993 <G00289865@atu.ie>
2022-12-06 16:18:18 +0000

    Added code to Passenger.java and PassengerSuccessTest.java

--- src/test/java/PassengerSuccessTest.java
+++ src/test/java/PassengerSuccessTest.java
@@ -1,15 +1,49 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PassengerSuccessTest {
 
+
+    private Passenger myPassenger;
+
     @BeforeEach
     void setUp() {
+        myPassenger = new Passenger ("Mr", "Lorcan Stakem", 1234567891, 12345678, 29 );
     }
 
+    @Test
+    void testTitlePass(){
+        assertEquals("Mr", myPassenger.getTitle());
+        myPassenger.setTitle("Ms");
+        assertEquals("Ms", myPassenger.getTitle());
+        myPassenger.setTitle("Mrs");
+        assertEquals("Mrs", myPassenger.getTitle());
+    }
+
+    @Test
+    void testNamePass() {
+        assertEquals("Lorcan Stakem", myPassenger.getName());
+    }
+
+    @Test
+    void testIdPass(){
+        assertEquals(1234567891, myPassenger.getId());
+    }
+
+    @Test
+    void testPhonePass () {
+        assertEquals(12345678, myPassenger.getPhone());
+    }
+
+@Test
+    void testAgePass() {
+        assertEquals(29, myPassenger.getAge());
+    }
     @AfterEach
     void tearDown() {
     }
+
 }
